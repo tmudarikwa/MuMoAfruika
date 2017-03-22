0977e76d600cc81d66d1e114cb63a55396f3f4f8
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-03-22 02:58:11 -0400

    page upgrapdes

--- public/js/emailinfocurrating.js
+++ public/js/emailinfocurrating.js
@@ -1,5 +1,10 @@
+$("#getemail").ready(function(){
+	//(".select2-selection__rendered").text(""); 
+	//alert("working");
+});
+
 $("footer a").on("mouseenter",function(){
-    $(this).css("color","#f80c00");
+    $(this).css("color","#f80c0MUSIC ARTIST0");
 	$(this).css("cursor","pointer");
 }).on("mouseleave",function(){
      $(this).css("color","white");
@@ -40,18 +45,34 @@ $(".userselection").on("select2:select",function(e) {
 	}
 
 });
+var defaultgetemailcontent = $("body section #getemail").html();
+$("header img").on("click",function(){
+		$("body section #getemail").html(defaultgetemailcontent);
+    });
 
-$("#emailin").on("click",function(){
-	  var format = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	  if(!format.test($(this).val()))
-	  {
-	  	alert("Please enter a valid email address");
-	  }
-
+$("footer a").on("click",function(){
+	var clickedvalue = $(this).html();
+	if(clickedvalue == " DEVELOPMENT PROGRESS ")
+	{
+		$("body section #selectioncontent").hide();
+		$("body section #getemail").html("<h1>"+clickedvalue+"</h1><p style='color:white'>testing</p>");
+	}
+	
+	else if (clickedvalue == " ABOUT MuMoAfruika")
+	{
+		$("body section #selectioncontent").hide();
+		$("body section #getemail").html("<h1>"+clickedvalue+"</h1><p style='color:white'>Imagine a music platform that has, at its core, music fans, music producers, and music artists as the main purpose of the platform. This is what's at the heart of MumoAfruika; a platform that is built to interplay at these main three categories of users. With tools that help each of the user types, enjoy streaming, sharing content for their users, and scouting for new talent. MumoAfruika is going to add a flare to the AfroBeat streaming scene.</p>");
+	}
 });
+
 
 $("#subscribe").on("click", function(){
 
+	  var format = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	  if(!format.test($("#emailin").val()))
+	  {
+	  	alert("Please enter a valid email address");
+	  }
 
 });
