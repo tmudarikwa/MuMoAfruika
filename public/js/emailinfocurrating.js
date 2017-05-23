786c1f92866131812d8956bb14dc3373c249c652
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-05-23 17:19:57 -0400

    minor text and css changes

--- public/js/emailinfocurrating.js
+++ public/js/emailinfocurrating.js
@@ -48,6 +48,21 @@ $(".userselection").on("select2:select",function(e) {
 	}
 
 });
+
+$("#subscribe").on("click", function(){
+	  var email = $("#emailin").val();
+	  var format = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	  if(format.test(email) == true)
+	  {
+	  	alert("Email address valid");
+	  }
+	  else 
+	  {
+	  	alert("Please enter a valid email address");
+	  }
+});
+
 var defaultgetemailcontent = $("body section #getemail").html();
 $("header img").on("click",function(){
 		$("body section #getemail").html(defaultgetemailcontent);
@@ -66,21 +81,4 @@ $("footer a").on("click",function(){
 		$("body section #selectioncontent").hide();
 		$("body section #getemail").html("<h1 style='color:#ffed01'>ABOUT US</h1><p>Imagine a music platform that has, at its core, music fans, music producers, and music artists as the main purpose of the platform. This is what's at the heart of MumoAfruika; a platform that is built to interplay on these main three categories of users. With tools that help each of the user types, enjoy streaming, sharing content for their users, and scouting for new talent. MumoAfruika is going to add a flare to the AfroBeat streaming scene.</p>");
 	}
-});
-
-
-$("#subscribe").on("click", function(){
-	  var email = $("#emailin").val();
-	  var format = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	  if(format.test(email) == true)
-	  {
-	  	alert("Email address valid");
-	  }
-	  else 
-	  {
-	  	alert("Please enter a valid email address");
-	  }
-
-	  console.log($("#emailin").val());
 });
