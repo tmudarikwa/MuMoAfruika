801d4ca6c2dddb8ae92359d76561905d25c9b823
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-05-29 02:28:00 -0400

    updated package.json

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.get('/unsubscribe/:email?', function(req,res){
 	});
 })
 
+app.use(flash()); // use connect-flash for flash messages stored in session
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
