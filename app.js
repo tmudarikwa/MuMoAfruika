e4f04ce3b5d2df55a93cb954f173e03f8720ebe6
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-06-02 01:41:08 -0400

    adding favicon

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // database configuration
 mongoose.connect(configDB.url);
 
@@ -24,8 +24,6 @@ mongoose.connect(configDB.url);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
