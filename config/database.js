6382aafdbebee4c05b3259f63de79e0ef7116a72
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-06-08 00:00:22 -0400

    development progress change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //databse configuration settings
 module.exports = {
 	
-	'url' : 'mongodb://heroku_6dd40tfq:nc476sqmbeb15ru1r6ntiev7nu@ds149551.mlab.com:49551/heroku_6dd40tfq',
+	'url' : 'mongodb://localhost/mumoafruika_test',
 };
