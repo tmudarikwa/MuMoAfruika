3e4d6f79ee10c84d123b61661ccb054f6d5f0f28
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-06-18 19:38:56 -0400

    gramma erros fixed on service types

--- public/js/emailinfocurrating.js
+++ public/js/emailinfocurrating.js
@@ -69,9 +69,9 @@ function userSelection(){
 		if(selection.includes("artist"))
 		{
 			selection = "an "+ "<b>" +selection.toUpperCase();
-			$("#selectioncontent").html("<p> As "+selection+"</b>" +" you will have a lot of features at your disposal. Development is currently underway and as we "+
-										"complete set milestones that are linked to these features we will send you emails. Furthermore, you will get access to"+
-										" upload your content when our beta pilot version of the platform see ready.<br/> Enter your email and click subscribe"+
+			$("#selectioncontent").html("<p> As "+selection+"</b>" +" you will have a lot of features at your disposal. Development is currently underway and as we have"+
+										"complete set milestones that are linked to these features, we will send you emails. Furthermore, you will get access to"+
+										" upload your content when our beta pilot version of the platform is ready.<br/> Enter your email and click subscribe"+
 								" to stay upto date with MuMoAfruika. </p> <input type='text' id='emailin' placeholder='ENTER EMAIL ADDRESS' value=''/> <br/> <button id='subscribe'> SUBSCRIBE</button");
 
 			subscribeHandling();
@@ -79,9 +79,9 @@ function userSelection(){
 		else if (selection.includes("producer"))
 		{
 			selection = "a "+ "<b>" +selection.toUpperCase();
-			$("#selectioncontent").html("<p> As "+selection+"</b>" +" you will have access to raw and emerging talent. But it doesn't end there, the tools that we "+
-										" are coining will further take the Abrobeat scene up a notch!"+
-										"<br/> Stay connected with our development progress and be the first to have access to our pilot test run of the system by subscribing to our email list."+
+			$("#selectioncontent").html("<p> As "+selection+"</b>" +" you will have access to raw and emerging talent, but it doesn't end there. The tools that we "+
+										" are coining will further take the Afrobeat scene up a notch!"+
+										"<br/> Stay connected with our development progress and be the first to have access to our beta pilot test run of the system by subscribing to our email list."+
 								" </p> <input type='text' id='emailin' placeholder='ENTER EMAIL ADDRESS' value=''/> <br/> <button id='subscribe'> SUBSCRIBE</button");
 			subscribeHandling();
 		}
@@ -89,7 +89,7 @@ function userSelection(){
 		{
 			selection = "a "+ "<b>" +selection.toUpperCase();
 			$("#selectioncontent").html("<p> As "+selection+"</b>" +" you will stay connected with the latest work from your favorite artists. We even have something"+
-																   " install to stay intouch with every concert they do! <br/>So go on ahead an subscribe to our email list"+
+																   " install for you to stay intouch with every concert they do! <br/>So go ahead and subscribe to our email list"+
 																   " and we will keep you updated with our development. <br/>"+
 																   "</p> <input type='text' id='emailin' placeholder='ENTER EMAIL ADDRESS' value=''/> <br/> <button id='subscribe'> SUBSCRIBE</button");
 			subscribeHandling();
