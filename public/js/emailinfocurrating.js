09c40d62b945f20b5a717b326d169cee73a778fa
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-07-27 20:53:17 -0400

    added an icon to the development progress page

--- public/js/emailinfocurrating.js
+++ public/js/emailinfocurrating.js
@@ -49,8 +49,8 @@ $("footer a").on("click",function(){
 	{
 		$("body .maincontent #selectioncontent").hide();
 		$(".maincontent").css("height","53.5vh");
-		$("body .maincontent #getemail").html("<h1 style='color:#ffed01'>DEVELOPMENT PROGRESS</h1>");
-		$("body .maincontent #getemail").append("<img src='images/development_icon.png' alt='Development Progress Icon'>");
+		$("body .maincontent #getemail").html("<img src='images/development_icon.png' alt='Development Progress Icon'>");
+		$("body .maincontent #getemail").append("<h1 style='color:#ffed01'>DEVELOPMENT PROGRESS</h1>");
 		$("body .cd-horizontal-timeline").show();
 		//$("body .cd-horizontal-timeline").html("<p style='width:100%;text-align:center'> development progress will be updated soon....</p>");
 	}
