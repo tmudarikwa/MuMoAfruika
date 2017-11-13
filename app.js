8c87ffaec2f3758f17ff5223d67f06190c02c95d
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-11-13 08:02:11 -0500

    adding CORS headers

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ mongoose.connect(configDB.url);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
