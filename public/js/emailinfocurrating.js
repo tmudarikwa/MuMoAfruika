0f8f611109e8fa5984a5958d82f2bd9e1b95e814
Takunda Mudarikwa <tkmud91@hotmail.com>
2017-12-04 02:35:08 -0500

    image responsiveness bootstrap hookup

--- public/js/emailinfocurrating.js
+++ public/js/emailinfocurrating.js
@@ -1,6 +1,7 @@
 /********************
- pop-up dialog close button handling
+ pop-up dialog close button handling & css
 ********************/
+$("#welcomedialog").css("margin-left",((((window.innerWidth - $("#welcomedialog").width()) / 2) / window.innerWidth) * 100) - 2);
 $("#close").click(function(){
 	$("#welcomedialog").hide();
 });
